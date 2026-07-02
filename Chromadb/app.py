5a59c7ad5af5150e4afb8487b596d0be5424a2d0
Jayanth Kumar Reddy <jayanthkumarreddy28@gmail.com>
2026-07-01 19:55:55 -0500

    Vector Stores

---
+++ Chromadb/app.py
@@ -0,0 +1,27 @@
+import chromadb
+
+chroma_client = chromadb.Client()
+
+collection = chroma_client.get_or_create_collection("mycollection")
+
+documents = [
+    {"id":"doc1","text":"Hello"},
+    {"id":"doc2","text":"How are you?"},
+    {"id":"doc3","text":"Goodbye, see you later!"}
+]
+
+query = "Hello, World!"
+
+for doc in documents:
+    collection.upsert(
+        documents=[doc["text"]],
+        ids=[doc["id"]],
+        metadatas=[{"source":"local","id":doc["id"]}]
+    )
+
+results = collection.query(
+    query_texts=query,
+    n_results=3
+)
+
+print(f"\n Results:",results, "\n\n")
